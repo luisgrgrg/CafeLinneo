a23854aa1356c95132df7f0743b35317d9a06859
luisgrgrg <luisgarogu@gmail.com>
2025-01-12 00:24:32 -0500

    First Deploy - Design Home - Section Hero

--- src/app/components/animations/animationTextDown.tsx
+++ src/app/components/animations/animationTextDown.tsx
@@ -2,14 +2,14 @@
 import { motion } from 'framer-motion'
 import { ReactNode } from 'react';
 
-interface AnimationTextDownProps { children: ReactNode; duration: number; }
-export const AnimationTextDown = ({ children, duration }: AnimationTextDownProps) => {
+interface AnimationTextDownProps { children: ReactNode; duration: number; delay: number  }
+export const AnimationTextDown = ({ children, duration, delay }: AnimationTextDownProps) => {
     return (
         <motion.h2
             className="flex justify-center items-center mb-4"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration }}
+            transition={{ duration, delay }}
         >
             {children}
         </motion.h2>
