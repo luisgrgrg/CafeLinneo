72b053585aedbdc03d4bba2897d717cb97fca58b
luisgrgrg <luisgarogu@gmail.com>
2025-01-12 02:32:21 -0500

    First Deploy - Design Home - Section Hero

--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -30,16 +30,17 @@ export default function Hero() {
                         </AnimationTextDown>
                         <AnimatedUp duration={0.95}>
                             <div className="flex flex-row justify-center items-center gap-10">
-                                <button className="bg-[#2B1D1D] text-white px-8 py-2 rounded-full border-2 border-[#C4A484] hover:opacity-90">
-                                    COMPRAR AHORA
-                                </button>
-
+                                <a href="https://wa.link/1ym7qr" target="_blank">
+                                    <button className="bg-[#2B1D1D] text-white px-8 py-2 rounded-full border-2 border-[#C4A484] hover:opacity-90">
+                                        COMPRAR AHORA
+                                    </button>
+                                </a>
                                 <button className="bg-[#2B1D1D] text-white px-8 py-2 rounded-full border-2 border-[#C4A484] hover:opacity-90">
                                     Nuestro Catálogo
                                 </button>
-                            </div> 
+                            </div>
                         </AnimatedUp>
-                    </div> 
+                    </div>
                     <AnimatedUp duration={0.8}>
                         <Image priority={true} className="w-auto lg:h-4/6 lg:ml-48 md:ml-80 md:h-2/6 sm:h-8 drop-shadow-4xl" src="/images/section-home-img-product.webp" alt="office content 2" width={600} height={400} />
                     </AnimatedUp >
