c688dd5fdceca99263947572fce90a17b69ff7c7
luisgrgrg <luisgarogu@gmail.com>
2025-01-28 22:09:25 -0500

    First Deploy - Design Home - Section Products

---
+++ src/app/components/footerHome.tsx
@@ -0,0 +1,8 @@
+"use client"
+export const FooterHome = () => {
+    return (
+        <div className="mx-auto full max-w-screen-xl py-4 py-6 lg:py-8">
+            <span>Footer</span>
+        </div>
+    )
+}
---
+++ src/app/data/productsDataCoffe.ts
@@ -0,0 +1,20 @@
+export const productsCoffe = [
+    {
+        img: "/images/products/cuartodelibra_cafe.webp",
+        title: "Cuarto de Libra",
+        precio: "12,000",
+        precioBefore: "15,000",
+    },
+    {
+        img: "/images/products/libra_cafe.webp",
+        title: "Libra - 454gr",
+        precio: "30,000",
+        precioBefore: "40,000",
+    },
+    {
+        img: "/images/products/cuartodelibra_cafe.webp",
+        title: "Cuarto de Libra",
+        precio: "18,000",
+        precioBefore: "25,000",
+    },
+];
